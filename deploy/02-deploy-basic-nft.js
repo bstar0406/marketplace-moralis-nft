295afb4c9b4fce644096b2d2a9795bd58fac027d
bstar0406 <handby256@outlook.com>
2018-10-19 14:04:00 +0900

    fake commit

---
+++ deploy/02-deploy-basic-nft.js
@@ -0,0 +1,37 @@
+const { network } = require("hardhat")
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    log("----------------------------------------------------")
+    const args = []
+    const basicNft = await deploy("BasicNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
+
+    const basicNftTwo = await deploy("BasicNftTwo", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(basicNft.address, args)
+        await verify(basicNftTwo.address, args)
+    }
+    log("----------------------------------------------------")
+}
+
+module.exports.tags = ["all", "basicnft"]
